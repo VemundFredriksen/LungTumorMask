7e25f52e4e0f5dc5f6ff047961fe48e345f59681
andreped <andrped94@gmail.com>
2022-03-18 13:18:33 +0000

    added new author and deps

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ setup(
     name="LungTumorMask",
     packages=find_packages(),
     version='1.0.1',
-    author="Svein Ole M Sevle, Vemund Fredriksen",
+    author="Svein Ole M Sevle, Vemund Fredriksen, Andre Pedersen",
     url="https://github.com/VemundFredriksen/LungTumorMask",
     license="MIT",
     python_requires='>=3.6',
+    install_requires=[
+        "numpy",
+        "monai",
+        "git+https://github.com/JoHof/lungmask",
+    ],
     entry_points={
         'console_scripts': [
             'lungtumormask = lungtumormask.__main__:main'
