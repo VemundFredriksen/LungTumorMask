549cc37ec6f7b098468eb1a4b0700f5db9031966
andreped <andrped94@gmail.com>
2022-05-19 15:00:37 +0200

    refactored

--- lungtumormask/mask.py
+++ lungtumormask/mask.py
@@ -11,7 +11,6 @@ def load_model():
         gpu_device = T.device('cpu')
     model = UNet_double(3, 1, 1, tuple([64, 128, 256, 512, 1024]), tuple([2 for i in range(4)]), num_res_units = 0)
     state_dict = T.hub.load_state_dict_from_url("https://github.com/VemundFredriksen/LungTumorMask/releases/download/0.0/dc_student.pth", progress=True, map_location=gpu_device)
-    #model.load_state_dict(T.load("D:\\OneDrive\\Skole\\Universitet\\10. Semester\\Masteroppgave\\bruk_for_full_model.pth", map_location="cuda:0"))
     model.load_state_dict(state_dict)
     model.eval()
     return model
@@ -28,8 +27,8 @@ def mask(image_path, save_path):
     right = model(preprocess_dump['right_lung']).squeeze(0).squeeze(0).detach().numpy()
 
     print("Post-processing image...")
-    infered = post_process(left, right, preprocess_dump).astype("uint8")
+    inferred = post_process(left, right, preprocess_dump).astype("uint8")
 
     print(f"Storing segmentation at {save_path}")
-    nimage = nibabel.Nifti1Image(infered, preprocess_dump['org_affine'])
+    nimage = nibabel.Nifti1Image(inferred, preprocess_dump['org_affine'])
     nibabel.save(nimage, save_path)
