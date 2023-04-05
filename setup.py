be27fc668a6488b9078b15aadd060616e1c0eaba
André Pedersen <andrped94@gmail.com>
2023-04-05 15:28:50 +0200

    use lungmask fork [no ci]

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         'numpy',
         'monai',
-        'lungmask@git+https://github.com/JoHof/lungmask',
+        'lungmask@git+https://github.com/andreped/lungmask',
         'nibabel',
     ],
     entry_points={
