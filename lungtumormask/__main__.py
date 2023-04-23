9921695fcaa6998bd9cde8bc0c93966fa241ebae
andreped <andrped94@gmail.com>
2023-04-23 16:01:38 +0200

    Threshold default 0.5 + morphology post-processing smoothing step

--- lungtumormask/__main__.py
+++ lungtumormask/__main__.py
@@ -14,7 +14,7 @@ def main():
     parser.add_argument('input', metavar='input', type=path, help='Path to the input image, should be .nifti')
     parser.add_argument('output', metavar='output', type=str, help='Filepath for output tumormask')
     parser.add_argument('--lung-filter', action='store_true', help='whether to apply lungmask postprocessing.')
-    parser.add_argument('--threshold', metavar='threshold', type=float, default=0.4, 
+    parser.add_argument('--threshold', metavar='threshold', type=float, default=0.5, 
                         help='which threshold to use for assigning voxel-wise classes.')
 
     argsin = sys.argv[1:]
